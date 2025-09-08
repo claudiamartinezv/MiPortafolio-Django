fabc0b2ab8e5af94c9baf11875f6eda0ccd1813c
claudiamartinezv <77359482+claudiamartinezv@users.noreply.github.com>
2025-09-08 16:51:25 -0300

    Actualización

--- mi_portafolio/settings.py
+++ mi_portafolio/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-from decouple import config, Csv
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,8 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 # Hosts permitidos
-#ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 
 # Application definition
 

76a7d8dc9d1ad312ccdbf17a33bd4ebd3326e413
claudiamartinezv <77359482+claudiamartinezv@users.noreply.github.com>
2025-09-08 20:04:11 -0300

    Actualización

--- mi_portafolio/settings.py
+++ mi_portafolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,12 +87,12 @@ WSGI_APPLICATION = 'mi_portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=config("DATABASE_URL", default="postgresql://postgres:cWgSDGgxiDEzjroCEWSttrWMBxICdIuU@postgres.railway.internal:5432/railway"),
+        conn_max_age=600,
+        ssl_require=False  # Si Railway pide SSL cambia a True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

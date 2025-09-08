42cfcdd35d2d43a2a9b3efaf3a307d3120368231
claudiamartinezv <77359482+claudiamartinezv@users.noreply.github.com>
2025-09-08 16:25:41 -0300

    Actualización

--- mi_portafolio/settings.py
+++ mi_portafolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 # Clave secreta
 SECRET_KEY = config("SECRET_KEY")
 
@@ -30,7 +31,8 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 # Hosts permitidos
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+#ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
 
 # Application definition
 
